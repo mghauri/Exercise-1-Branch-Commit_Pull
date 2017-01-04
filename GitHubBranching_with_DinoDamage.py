45d249162fdb2b4359ba03c03280f581d01b3ea0
Cooper French <cooperii@uw.edu>
2017-01-04 07:45:44 -0800

    Practice Branching Files in both .py and .ipynb

---
+++ GitHubBranching_with_DinoDamage.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+# Welcome to Python
+# Hashes (#) are used to comment text.
+# Any line with a hash in fornt of it will not run
+
+
+# In[ ]:
+
+import numpy as np
+
